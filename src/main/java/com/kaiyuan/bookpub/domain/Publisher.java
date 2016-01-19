38383d1f2f25b12d8290e4759ff6ce0584661780
Kaiyuan Liu <liu.k@gameonmedia.ie>
2016-01-19 10:07:31 +0000

    move Reviewer into an entity from Publisher

--- src/main/java/com/kaiyuan/bookpub/domain/Publisher.java
+++ src/main/java/com/kaiyuan/bookpub/domain/Publisher.java
@@ -55,49 +55,4 @@ public class Publisher {
             ", books=" + books +
             '}';
     }
-
-    @Entity
-    public class Reviewer {
-        @Id
-        @GeneratedValue
-        private Long id;
-        private String firstName;
-        private String lastName;
-
-        protected Reviewer() {}
-
-        public Reviewer(String firstName, String lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        @Override
-        public String toString() {
-            return "Reviewer{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-        }
-    }
 }
